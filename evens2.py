2e07ee3edc8ba3d3a9a18560cf214149b5e8ca65
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-25 15:40:58 +0000

    Added new test driven development file

---
+++ evens2.py
@@ -0,0 +1,7 @@
+def even_number_of_evens(numbers):
+    return False
+    
+assert even_number_of_evens([]) == False, "No numbers"
+assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+
+print("All tests passed")
