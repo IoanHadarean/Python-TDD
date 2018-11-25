41ca723788ba159c8d5dbb02ad5ce45999b12d69
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-25 17:52:14 +0000

    Added idiomatic Pythn

--- evens2.py
+++ evens2.py
@@ -1,15 +1,12 @@
+def is_even(number):
+    return number % 2 == 0
+
+
 def even_number_of_evens(numbers):
-    
-    evens = 0
-        
-    for x in numbers:
-        if x % 2 == 0:
-            evens += 1
-    
-    if evens == 0:
-        return False
-    else:
-        return evens % 2 == 0
+   
+   evens = sum([1 for n in numbers if is_even(n)])
+   return False if evens == 0 else is_even(evens)
+   
     
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2, 4]) == True, "Two even numbers"
