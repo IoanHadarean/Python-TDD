37c8d576c1f196925cfcb9988374e785847e3392
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-25 15:53:13 +0000

    Added code to make the third test pass

--- evens2.py
+++ evens2.py
@@ -2,7 +2,13 @@ def even_number_of_evens(numbers):
     if numbers == []:
         return False
     else:
-        return True
+        evens = 0
+        
+    for x in numbers:
+        if x % 2 == 0:
+            evens +=1
+            
+    return evens % 2 == 0
     
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2, 4]) == True, "Two even numbers"
