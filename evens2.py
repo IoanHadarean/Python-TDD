cd1e72774c0e5379c67d248b01729e7c8dff57eb
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-25 15:58:58 +0000

    Added code in roder to make the last test pass

--- evens2.py
+++ evens2.py
@@ -6,9 +6,12 @@ def even_number_of_evens(numbers):
         
     for x in numbers:
         if x % 2 == 0:
-            evens +=1
-            
-    return evens % 2 == 0
+            evens += 1
+    
+    if evens == 0:
+        return False
+    else:
+        return evens % 2 == 0
     
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2, 4]) == True, "Two even numbers"
