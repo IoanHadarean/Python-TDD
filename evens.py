98444dc20ae1a9d3070c580fc93996257aafe6fc
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-25 15:11:57 +0000

    4 tests passed

--- evens.py
+++ evens.py
@@ -25,6 +25,8 @@ def even_number_of_evens(numbers):
         return False
     elif(evens % 2 == 1):
         return False
+    elif(evens % 2 == 0):
+        return True
 
     
             
@@ -32,12 +34,13 @@ def even_number_of_evens(numbers):
 
 
 assert even_number_of_evens([]) == False, "No numbers"
+assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 assert even_number_of_evens([2]) == False, "One even number"
-#assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+assert even_number_of_evens([2, 4]) == True, "Two even numbers"
 #assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
 #assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
 #assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three are even"
 #assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four are even"
-assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
+
 
 print("All tests passed")
