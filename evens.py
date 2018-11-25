70c5b1afc9ea2f522c5caf91ba059d07df4506db
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-25 14:48:15 +0000

    Made first test to pass

--- evens.py
+++ evens.py
@@ -1,5 +1,16 @@
 def even_number_of_evens(numbers):
-    return False
+     """
+    Returns the number of even numbers contained in a list of numbers.
+    `numbers` should be a list containing numbers
+    
+    Returns either True or False based on a number of criteria.
+        - if `numbers` is empty, return `False`
+        - if the number of even numbers is odd, return `False`
+        - if the number of even numbers is 0, return `False`
+        - if the number of even numbers is even, return `True`
+    """
+    if(numbers == []):
+        return False
 
 
 assert even_number_of_evens([]) == False, "No numbers"
