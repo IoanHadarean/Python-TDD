6bf91731204af99a3ae7b166cf93985da5024aa2
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-01 12:19:30 +0000

    Did failing test for test_is_in

--- byotest.py
+++ byotest.py
@@ -10,5 +10,8 @@ def test_not_equal(a, b):
 def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(collection, item)
     
+"""test_are_equal(number_of_evens([1,2,3,4,5]), 2)"""   
     
-test_not_equal(number_of_evens([1,2,3,4,5]), 0)
+"""test_not_equal(number_of_evens([1,2,3,4,5]), 0)"""
+
+test_is_in(["fruit", "banana"], "apple")
