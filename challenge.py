5384094e9aee09ee728580e1c82d8824b3290c3a
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-01 19:10:56 +0000

    Added challenge algorithm and testing

--- challenge.py
+++ challenge.py
@@ -4,15 +4,17 @@ usd_coins = {100:20,50:20,25:20,10:20,5:20,1:20}
 eur_coins = {200:20,100:20,50:20,10:20,5:20,2:20,1:20}
 
 
-def get_change(amount, coins):
-    for item in coins:
-        return item
+def get_change(amount, coins=eur_coins):
     
     change = []
-    for coin in item:
-        while coin <= amount:
+    for coin in sorted(coins.keys(), reverse = True) :
+        while coin <= amount and coins[coin] > 0:
             amount -= coin
+            coins[coin] -= 1
             change.append(coin)
+    
+    if amount != 0:
+        raise Exception ("Insuficient funds")
             
     return change
         
@@ -28,7 +30,9 @@ test_are_equal(get_change(100),[100])
 test_are_equal(get_change(3),[2,1])
 test_are_equal(get_change(7),[5,2])
 test_are_equal(get_change(9), [5,2,2])
-test_are_equal(get_change(35, item),[25, 10])
+test_are_equal(get_change(35, usd_coins),[25, 10])
+test_are_equal(get_change(4), {2:1,1:4})
+test_are_equal(get_change(4), {5:1,2:1,1:1})
 
 print("All tests pass!")
 
